569546213bf8124e23b5bc7958a5434601099241
unknown <flavinhaalmeiiida234@gmail.com>
2023-07-28 23:17:39 -0300

    att index

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const express = require('express');
 const api = require("./api");
 const app = express();
 
-app.set("views", "./static/");
-app.use(express.static("./public/"))
-app.engine('html', require('ejs').renderFile);
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
