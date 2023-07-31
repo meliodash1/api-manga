801ded9bd4958f3cd57da5aa5e32c316fb599e64
unknown <flavinhaalmeiiida234@gmail.com>
2023-07-31 00:33:16 -0300

    att index.js proxy

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express();
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use(
     '/api', // Caminho de origem para o proxy (defina conforme suas necessidades)
     createProxyMiddleware({
